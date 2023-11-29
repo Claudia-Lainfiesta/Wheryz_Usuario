6bc3ee34f30a3619e5bed9d143b9cc058eda9afe
Usuario <mclainfiestaherrera@gmail.com>
2023-11-28 18:54:45 -0600

    Hecho configuración

---
+++ js/configuracion.js
@@ -0,0 +1,7 @@
+function open_change_image() {
+    document.getElementById("desplegar-cambiar-foto").removeAttribute("hidden");
+}
+
+function close_change_image() {
+    document.getElementById("desplegar-cambiar-foto").setAttribute("hidden", "true");
+}
