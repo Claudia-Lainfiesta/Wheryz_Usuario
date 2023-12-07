5ca3fd793b9f15049f0d48c431af2df18d61a11c
Usuario <mclainfiestaherrera@gmail.com>
2023-12-07 17:11:40 -0600

    Arreglo de errores favs

--- connection/favoritoss.php
+++ connection/favoritoss.php
@@ -6,27 +6,38 @@ $database = "db_wheryz";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
+// Incluir aquí tu lógica de conexión a la base de datos
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id_usuario = $_POST["id_usuario"];
-    $id_colaborador = $_POST["id_colaborador"];
-    $isFavorito = $_POST["isFavorito"];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id_usuario = $_POST['id_usuario'];
+    $id_colaborador = $_POST['id_colaborador'];
+    $isFavorito = $_POST['isFavorito'];
 
-    if ($isFavorito == "true") {
-        $sql = "DELETE FROM tb_usuario_favoritos WHERE id_usuario = $id_usuario AND id_colaborador = $id_colaborador";
+    // Verificar si ya existe el registro en la tabla de favoritos
+    $query = "SELECT * FROM tb_usuario_favoritos WHERE id_usuario = ? AND id_colaborador = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("ii", $id_usuario, $id_colaborador);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // Si ya existe, eliminar el registro
+        $deleteQuery = "DELETE FROM tb_usuario_favoritos WHERE id_usuario = ? AND id_colaborador = ?";
+        $deleteStmt = $conn->prepare($deleteQuery);
+        $deleteStmt->bind_param("ii", $id_usuario, $id_colaborador);
+        $deleteStmt->execute();
+        echo "Eliminado de favoritos";
     } else {
-        $sql = "INSERT INTO tb_usuario_favoritos (id_usuario, id_colaborador) VALUES ($id_usuario, $id_colaborador)";
+        // Si no existe, agregar el registro
+        $insertQuery = "INSERT INTO tb_usuario_favoritos (id_usuario, id_colaborador) VALUES (?, ?)";
+        $insertStmt = $conn->prepare($insertQuery);
+        $insertStmt->bind_param("ii", $id_usuario, $id_colaborador);
+        $insertStmt->execute();
+        echo "Agregado a favoritos";
     }
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Operación exitosa";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    $stmt->close();
+    $conn->close();
 }
-
-$conn->close();
 ?>
+
